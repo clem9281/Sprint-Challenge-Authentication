81bfae0cff5e5398fab19e46a2f0ff179ad62f46
clem9281 <clem9281@gmail.com>
2019-04-05 08:56:43 -0700

    finished user register endpoint

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,6 @@
 const axios = require('axios');
+const bcrypt = require('bcryptjs');
+const db = require('../database/dbHelpers');
 
 const { authenticate } = require('../auth/authenticate');
 
@@ -8,9 +10,25 @@ module.exports = server => {
   server.get('/api/jokes', authenticate, getJokes);
 };
 
-function register(req, res) {
+async function register(req, res) {
   // implement user registration
+  const { username, password } = req.body;
+  if (!username || !password) return res.status(400).json({ message: "You must have a username and password to register a user" })
+  const hash = bcrypt.hashSync(password);
+  req.body.password = hash;
+  try {
+    const newUserId = await db.addUser(req.body);
+    const newUser = await db.findBy({ id: newUserId[0] }).first();
+    const newUserWithoutPassword = {
+      id: newUser.id,
+      username: newUser.username
+    }
+    res.status(201).json(newUserWithoutPassword);
+  } catch (error) {
+    res.status(500).json({ message: "We could not register a user at this time" })
+  }
 }
+
 
 function login(req, res) {
   // implement user login
